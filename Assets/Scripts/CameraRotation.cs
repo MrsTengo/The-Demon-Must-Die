f94ab951b774b95e1fa74a9654353ab0f4022458
MrsTengo <161305601+MrsTengo@users.noreply.github.com>
2024-02-28 22:29:39 +0300

    Camera Rotation 1

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    public Transform cameraAxisTransform;
+
+    public float rotationSpeed;
+    public float minAngle;
+    public float maxAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime*rotationSpeed*Input.GetAxis("Mouse X"), 0);
+
+        var newAngleX = cameraAxisTransform.localEulerAngles.x - Time.deltaTime * rotationSpeed * Input.GetAxis("Mouse Y");
+        newAngleX = Mathf.Clamp(newAngleX, minAngle, maxAngle);
+
+        Debug.Log(newAngleX);
+
+        cameraAxisTransform.localEulerAngles = new Vector3(newAngleX , 0, 0);
+    }
+}
